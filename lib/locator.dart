9cef1af96d872c879e13570844debc094333c6e5
Samuel Mataraso <samuelmataraso@gmail.com>
2019-11-16 13:05:49 -0300

    added more locators

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,13 @@
+import 'package:flutter_provider_architecture/core/services/api.dart';
+import 'package:flutter_provider_architecture/core/services/authentication_service.dart';
 import 'package:flutter_provider_architecture/core/viewmodels/login_model.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
+  locator.registerLazySingleton(() => AuthenticationService());
+  locator.registerLazySingleton(() => Api());
+
   locator.registerLazySingleton(() => LoginModel());
 }
