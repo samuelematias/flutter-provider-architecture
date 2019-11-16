f6cf4ed3f36bd1c21234cebb933ddbb463656273
Samuel Mataraso <samuelmataraso@gmail.com>
2019-11-16 17:47:02 -0300

    Implementing a way to start some func when the model is ready.

--- lib/ui/views/base_view.dart
+++ lib/ui/views/base_view.dart
@@ -3,16 +3,35 @@ import 'package:flutter_provider_architecture/core/viewmodels/base_model.dart';
 import 'package:flutter_provider_architecture/locator.dart';
 import 'package:provider/provider.dart';
 
-class BaseView<T extends BaseModel> extends StatelessWidget {
+class BaseView<T extends BaseModel> extends StatefulWidget {
   final Widget Function(BuildContext context, T model, Widget child) builder;
+  final Function(T) onModelReeady;
 
-  BaseView({this.builder});
+  BaseView({
+    this.builder,
+    this.onModelReeady,
+  });
+
+  @override
+  _BaseViewState<T> createState() => _BaseViewState<T>();
+}
+
+class _BaseViewState<T extends BaseModel> extends State<BaseView<T>> {
+  T model = locator<T>();
+
+  @override
+  void initState() {
+    if (widget.onModelReeady != null) {
+      widget.onModelReeady(model);
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<T>(
-      builder: (BuildContext context) => locator<T>(),
-      child: Consumer<T>(builder: builder),
+      builder: (BuildContext context) => model,
+      child: Consumer<T>(builder: widget.builder),
     );
   }
 }
