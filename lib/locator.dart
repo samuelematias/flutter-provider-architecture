f81cd63014c7e2f2092a45cdb265b88c58a20617
Samuel Mataraso <samuelmataraso@gmail.com>
2019-11-16 12:40:32 -0300

    added login model on locator.

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_provider_architecture/core/viewmodels/login_model.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 
-void setupLocator() {}
+void setupLocator() {
+  locator.registerLazySingleton(() => LoginModel());
+}
