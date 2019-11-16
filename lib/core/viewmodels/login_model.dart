31a24babe7622f325f142f2660d773c39b3a7179
Samuel Mataraso <samuelmataraso@gmail.com>
2019-11-16 13:05:24 -0300

    create the state management to login model

--- lib/core/viewmodels/login_model.dart
+++ lib/core/viewmodels/login_model.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider_architecture/core/services/authentication_service.dart';
+import 'package:flutter_provider_architecture/locator.dart';
 
 enum ViewState { Idle, Busy }
 
-class LoginModel extends ChangeNotifier {}
+class LoginModel extends ChangeNotifier {
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
+
+  ViewState _state = ViewState.Idle;
+
+  ViewState get state => _state;
+
+  void setState(ViewState viewState) {
+    _state = viewState;
+    notifyListeners();
+  }
+
+  Future<bool> login(String userIdText) async {
+    setState(ViewState.Busy);
+    var userId = int.tryParse(userIdText);
+    var success = await _authenticationService.login(userId);
+
+    setState(ViewState.Idle);
+
+    return success;
+  }
+}
